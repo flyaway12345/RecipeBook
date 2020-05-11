412c6abc183164f795a694499e48187d280e5ae0
Andrew Oluoch <oluoch3@gmail.com>
2020-05-11 13:34:18 -0400

    i recipe-cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RecipeItemComponent } from "./body/recipe-item/recipe-item.component";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
 import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -14,7 +13,9 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    RecipeItemComponent,
+    
   ],
   imports: [
     BrowserModule,
